3d1761e7377b546ccc4907e24f5bde83c49fc178
isbye01 <isbaichik01@gmail.com>
2025-09-09 12:08:42 +0300

    fix eslint and add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,30 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
+	globalIgnores(['dist', 'public']),
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs['recommended-latest'],
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			ecmaVersion: 2023,
+			globals: globals.browser,
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			reactRefresh: reactRefresh,
+		},
+		rules: {
+			...reactHooks.configs.recommenden.rules,
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		},
+	},
+	eslintConfigPrettier,
 ])
